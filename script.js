4055ce162461587d04fca56733196680a262b6fd
kaushikmondalwebskitters <98884776+kaushikmondalwebskitters@users.noreply.github.com>
2024-03-19 11:25:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,16 +4,16 @@
 
 jQuery(document).ready(function ($) {
 
-     // Smooth Scroll
-     const lenis = new Lenis()
-     lenis.on('scroll', (e) => {
-         console.log(e)
-     })
-     function raf(time) {
-         lenis.raf(time)
-         requestAnimationFrame(raf)
-     }
-     requestAnimationFrame(raf)
+    // Smooth Scroll
+    const lenis = new Lenis()
+    lenis.on('scroll', (e) => {
+        console.log(e)
+    })
+    function raf(time) {
+        lenis.raf(time)
+        requestAnimationFrame(raf)
+    }
+    requestAnimationFrame(raf)
 
     if ($('.building-manufacturing-slider').length) {
         $(".building-manufacturing-slider").slick({
@@ -34,7 +34,39 @@ jQuery(document).ready(function ($) {
 
 
     function afterLoad() {
-        alert('sd')
+        let page_container = document.querySelector('.supersede-body');
+        //// dynamic text animation -- by lines
+        if ($("[data-char='intro']").length) {
+            $("[data-char='intro']").each(function () {
+                let els = $(this);
+                let txt_target2 = els.find(".word_wrapper");
+                let txt_target3 = els.find(".word");
+                gsap.to(els, 0.5, {
+                    scrollTrigger: {
+                        trigger: els,
+                        start: "top bottom",
+                        end: "bottom bottom",
+                        scroller: page_container,
+                        scrub: false,
+                        //  markers: true,
+                    },
+                    onComplete: function () {
+                        gsap.to(txt_target2, 1, {
+                            scaleY: 1,
+                            yPercent: 0,
+                            rotationX: 0,
+                            stagger: 0.05,
+                            transformOrigin: "center bottom",
+                            transformStyle: "preserve-3d",
+                            ease: Power3.easeOut,
+                        });
+                        gsap.to(txt_target3, 1, {
+                            opacity: 1,
+                        });
+                    },
+                });
+            });
+        }
     }
 
 
